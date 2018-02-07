ce05038bae04bcde05bd3870aa932bbb03f18aaf
WORKMA20 <WORKMA20@JB217-L09-W-D>
2018-02-07 16:39:31 -0500

    Add project files.

---
+++ Demo_TheTravelingSalesperson.S1_Starter/Controllers/Controller.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_TheTravelingSalesperson
+{
+    public class Controller
+    {
+       private ConsoleView _consoleView;
+       private Salesperson _salesperson;
+       private bool _usingApplication;
+
+        public Controller()
+        {
+            InitializeController();
+
+            _salesperson = new Salesperson();
+
+            _consoleView = new ConsoleView();
+
+            ManageApplicationLoop();
+        }
+
+        public void DisplayAccountInfo()
+        {
+            _consoleView.DisplayAccountInfo(_salesperson);
+        }
+
+        public void DisplayCities()
+        {
+            _consoleView.DisplayCitiesTraveled(_salesperson);
+        }
+
+        public void InitializeController()
+        {
+            _usingApplication = true;
+        }
+
+        public void ManageApplicationLoop()
+        {
+            MenuOption userMenuChoice;
+
+            _consoleView.DisplayWelcomeScreen();
+
+            _salesperson = _consoleView.DisplaySetupAccount();
+
+            while (_usingApplication)
+            {
+
+                userMenuChoice = _consoleView.DisplayGetUserMenuChoice();
+
+                switch (userMenuChoice)
+                {
+                    case MenuOption.None:
+                        break;
+                    case MenuOption.Travel:
+                        Travel();
+                        break;
+                    case MenuOption.Buy:
+                        Buy();
+                        break;
+                    case MenuOption.Sell:
+                        Sell();
+                        break;
+                    case MenuOption.DisplayInventory:
+                        DisplayInventory();
+                        break;
+                    case MenuOption.DisplayCities:
+                        DisplayCities();
+                        break;
+                    case MenuOption.DisplayAccountInfo:
+                        DisplayAccountInfo();
+                        break;
+                    case MenuOption.SaveAccountInfo:
+                        SaveAccountInfo();
+                        break;
+                    case MenuOption.LoadAccountInfo:
+                        LoadAccountInfo();
+                        break;
+                    case MenuOption.Exit:
+                        _usingApplication = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            _consoleView.DisplayClosingScreen();
+
+            Environment.Exit(1);
+
+        }
+
+        ///
+        /// Buy product
+        /// 
+        private void Buy()
+        {
+            int numberOfUnits = _consoleView.DisplayGetNumberOfUnitsToBuy(_salesperson.CurrentStock);
+            _salesperson.CurrentStock.AddProducts(numberOfUnits);
+        }
+
+        /// <summary>
+        /// Sell Products
+        /// </summary>
+        private void Sell()
+        {
+            int numberOfUnits = _consoleView.DisplayGetNumberOfUnitsToSell(_salesperson.CurrentStock);
+            _salesperson.CurrentStock.SubtractProducts(numberOfUnits);
+
+            if (_salesperson.CurrentStock.OnBackOrder)
+            {
+                _consoleView.DisplayBackorderNotification(_salesperson.CurrentStock, numberOfUnits);
+            }
+        }
+
+        private void DisplayInventory()
+        {
+            _consoleView.DisplayInventory(_salesperson.CurrentStock);
+        }
+
+        public void Travel()
+        {
+            string nextCity = _consoleView.DisplayGetNextCity();
+            _salesperson.CitiesVisited.Add(nextCity);
+        }
+
+        private void SaveAccountInfo()
+        {
+
+        }
+
+        private void LoadAccountInfo()
+        {
+
+        }
+    }
+}
---
+++ Demo_TheTravelingSalesperson.S1_Starter/Views/ConsoleView.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_TheTravelingSalesperson
+{
+    public class ConsoleView
+    {
+
+        private const int MAXIMUM_ATTEMPTS = 3;
+        private const int MINIMUM_BUYSELL_AMOUNT = 0;
+        private const int MAXIMUM_BUYSELL_AMOUNT = 100;
+
+        public ConsoleView()
+        {
+            InitializeConsole();
+        }
+
+        private void InitializeConsole()
+        {
+            ConsoleUtil.WindowTitle = "Northern Woods Productions";
+            ConsoleUtil.HeaderText = "The Traveling Salesperson Application";
+        }
+
+
+        public void DisplayContinuePrompt()
+        {
+            Console.CursorVisible = false;
+
+            ConsoleUtil.DisplayMessage("");
+
+            ConsoleUtil.DisplayMessage("Press any key to continue.");
+            ConsoleKeyInfo response = Console.ReadKey();
+
+            ConsoleUtil.DisplayMessage("");
+
+            Console.CursorVisible = true;
+        }
+
+
+        public void DisplayExitPrompt()
+        {
+            ConsoleUtil.DisplayReset();
+
+            Console.CursorVisible = false;
+
+            ConsoleUtil.DisplayMessage("");
+            ConsoleUtil.DisplayMessage("Thank you for using the application. Press any key to Exit.");
+
+            Console.ReadKey();
+
+            System.Environment.Exit(1);
+        }
+
+        public void DisplayInventory(Product product)
+        {
+            ConsoleUtil.HeaderText = "Current Inventory";
+            ConsoleUtil.DisplayReset();
+
+            ConsoleUtil.DisplayMessage("Product type: " + product.Type.ToString());
+            ConsoleUtil.DisplayMessage("Number of units: " + product.NumberOfUnits.ToString());
+            ConsoleUtil.DisplayMessage("");
+
+            DisplayContinuePrompt();
+        }
+
+        public void DisplayWelcomeScreen()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            ConsoleUtil.DisplayReset();
+
+            ConsoleUtil.DisplayMessage("Written by Tyler Workman");
+            ConsoleUtil.DisplayMessage("Northwestern Michigan College");
+            ConsoleUtil.DisplayMessage("");
+
+            sb.Clear();
+            sb.AppendFormat("You are a traveling salesperson buying and selling widgets ");
+            sb.AppendFormat("around the country. You will be prompted regarding which city ");
+            sb.AppendFormat("you wish to travel to and will then be asked whether you wish to buy ");
+            sb.AppendFormat("or sell widgets.");
+            ConsoleUtil.DisplayMessage(sb.ToString());
+            ConsoleUtil.DisplayMessage("");
+
+            sb.Clear();
+            sb.AppendFormat("Your first task will be to set up your account details.");
+            ConsoleUtil.DisplayMessage(sb.ToString());
+
+            DisplayContinuePrompt();
+        }
+    
+
+        public Salesperson DisplaySetupAccount()
+        {
+            Salesperson salesperson = new Salesperson();
+
+            ConsoleUtil.HeaderText = "Account Setup";
+            ConsoleUtil.DisplayReset();
+
+            ConsoleUtil.DisplayMessage("Setup your account now.");
+            ConsoleUtil.DisplayMessage("");
+
+            ConsoleUtil.DisplayPromptMessage("Enter your first name: ");
+            salesperson.FirstName = Console.ReadLine();
+            ConsoleUtil.DisplayMessage("");
+
+            ConsoleUtil.DisplayPromptMessage("Enter your last name: ");
+            salesperson.LastName = Console.ReadLine();
+            ConsoleUtil.DisplayMessage("");
+
+            ConsoleUtil.DisplayPromptMessage("Enter your account ID: ");
+            salesperson.AccountID = Console.ReadLine();
+            ConsoleUtil.DisplayMessage("");
+
+            ConsoleUtil.DisplayMessage("Product Types");
+            ConsoleUtil.DisplayMessage("");
+
+            ConsoleUtil.DisplayMessage("");
+
+            ConsoleUtil.DisplayReset();
+
+            ConsoleUtil.DisplayMessage("Your account is setup");
+
+            DisplayContinuePrompt();
+
+            return salesperson;
+        }
+
+
+        
+
+
+public void DisplayClosingScreen()
+        {
+            ConsoleUtil.DisplayReset();
+
+            ConsoleUtil.DisplayMessage("Thank you for using The Traveling Salesperson Application.");
+
+            DisplayContinuePrompt();
+        }
+
+
+        public MenuOption DisplayGetUserMenuChoice()
+        {
+            MenuOption userMenuChoice = MenuOption.None;
+            bool usingMenu = true;
+
+            while (usingMenu)
+            {
+
+                ConsoleUtil.DisplayReset();
+                Console.CursorVisible = false;
+
+
+                ConsoleUtil.DisplayMessage("Please type the number of your menu choice.");
+                ConsoleUtil.DisplayMessage("");
+                Console.Write(
+                    "\t" + "1. Travel" + Environment.NewLine +
+                    "\t" + "2. Buy" + Environment.NewLine +
+                    "\t" + "3. Sell" + Environment.NewLine +
+                    "\t" + "4. Display Inventory" + Environment.NewLine +
+                    "\t" + "5. Display Cities" + Environment.NewLine +
+                    "\t" + "6. Display Account Info" + Environment.NewLine +
+                    "\t" + "E. Exit" + Environment.NewLine);
+
+
+                ConsoleKeyInfo userResponse = Console.ReadKey(true);
+                switch (userResponse.KeyChar)
+                {
+                    case '1':
+                        userMenuChoice = MenuOption.Travel;
+                        usingMenu = false;
+                        break;
+                    case '2':
+                        userMenuChoice = MenuOption.Buy;
+                        usingMenu = false;
+                        break;
+                    case '3':
+                        userMenuChoice = MenuOption.Sell;
+                        usingMenu = false;
+                        break;
+                    case '4':
+                        userMenuChoice = MenuOption.DisplayInventory;
+                        usingMenu = false;
+                        break;
+                    case '5':
+                        userMenuChoice = MenuOption.DisplayCities;
+                        usingMenu = false;
+                        break;
+                    case '6':
+                        userMenuChoice = MenuOption.DisplayAccountInfo;
+                        usingMenu = false;
+                        break;
+                    case 'E':
+                    case 'e':
+                        userMenuChoice = MenuOption.Exit;
+                        usingMenu = false;
+                        break;
+                    default:
+                        ConsoleUtil.DisplayMessage(
+                            "It appears you have selected an incorrect choice." + Environment.NewLine +
+                            "Press any key to continue or the ESC key to quit the application.");
+
+                        userResponse = Console.ReadKey(true);
+                        if (userResponse.Key == ConsoleKey.Escape)
+                        {
+                            usingMenu = false;
+                        }
+                        break;
+                }
+            }
+            Console.CursorVisible = true;
+
+            return userMenuChoice;
+        }
+
+        public string DisplayGetNextCity()
+        {
+            string nextCity = "";
+
+            ConsoleUtil.DisplayReset();
+
+            ConsoleUtil.DisplayPromptMessage("Enter the name of the next city:");
+            nextCity = Console.ReadLine();
+
+            return nextCity;
+        }
+
+        public int DisplayGetNumberOfUnitsToBuy(Product product)
+        {
+            ConsoleUtil.HeaderText = "Buy Inventory";
+            ConsoleUtil.DisplayReset();
+
+            //
+            // get number of products to buy
+            //
+            ConsoleUtil.DisplayMessage("Buying " + product.Type.ToString() + " products.");
+            ConsoleUtil.DisplayMessage("");
+
+            if (!ConsoleValidator.TryGetIntegerFromUser(MINIMUM_BUYSELL_AMOUNT, MAXIMUM_BUYSELL_AMOUNT, MAXIMUM_ATTEMPTS, "products", out int numberOfUnitsToBuy))
+            {
+                ConsoleUtil.DisplayMessage("It appears you are having difficulty setting the number of products to buy.");
+                ConsoleUtil.DisplayMessage("By default, the number of products to sell will be set to zero.");
+                numberOfUnitsToBuy = 0;
+                DisplayContinuePrompt();
+            }
+
+            ConsoleUtil.DisplayReset();
+
+            ConsoleUtil.DisplayMessage(numberOfUnitsToBuy + " " + product.Type.ToString() + " products have been added to the inventory.");
+
+            DisplayContinuePrompt();
+
+            return numberOfUnitsToBuy;
+        }
+
+        public int DisplayGetNumberOfUnitsToSell(Product product)
+        {
+            ConsoleUtil.HeaderText = "Sell Inventory";
+            ConsoleUtil.DisplayReset();
+
+            //
+            // get number of products to sell
+            //
+            ConsoleUtil.DisplayMessage("Selling " + product.Type.ToString() + " products.");
+            ConsoleUtil.DisplayMessage("");
+
+            if (!ConsoleValidator.TryGetIntegerFromUser(MINIMUM_BUYSELL_AMOUNT, MAXIMUM_BUYSELL_AMOUNT, MAXIMUM_ATTEMPTS, "products", out int numberOfUnitsToSell))
+            {
+                ConsoleUtil.DisplayMessage("It appears you are having difficulty setting the number of products to sell.");
+                ConsoleUtil.DisplayMessage("By default, the number of products to sell will be set to zero.");
+                numberOfUnitsToSell = 0;
+                DisplayContinuePrompt();
+            }
+
+            ConsoleUtil.DisplayReset();
+
+            ConsoleUtil.DisplayMessage(numberOfUnitsToSell + " " + product.Type.ToString() + " products have been subtracted from the inventory.");
+
+            DisplayContinuePrompt();
+
+            return numberOfUnitsToSell;
+        }
+
+        public void DisplayBackorderNotification(Product product, int numberOfUnitsSold)
+        {
+            ConsoleUtil.HeaderText = "Inventory Backorder Notification";
+            ConsoleUtil.DisplayReset();
+
+            int numberOfUnitsBackordered = Math.Abs(product.NumberOfUnits);
+            int numberOfUnitsShipped = numberOfUnitsSold - numberOfUnitsBackordered;
+
+            ConsoleUtil.DisplayMessage("Products Sold: " + numberOfUnitsSold);
+            ConsoleUtil.DisplayMessage("Products Shipped: " + numberOfUnitsShipped);
+            ConsoleUtil.DisplayMessage("Products on Backorder: " + numberOfUnitsBackordered);
+
+            DisplayContinuePrompt();
+        }
+
+public void DisplayCitiesTraveled(Salesperson salesperson)
+        {
+            ConsoleUtil.DisplayReset();
+
+            ConsoleUtil.DisplayMessage("You have traveled to the following cities.");
+            ConsoleUtil.DisplayMessage("");
+
+            foreach (string city in salesperson.CitiesVisited)
+            {
+                ConsoleUtil.DisplayMessage(city);
+            }
+
+            DisplayContinuePrompt();
+        }
+
+
+        public void DisplayAccountInfo(Salesperson salesperson)
+        {
+            ConsoleUtil.HeaderText = "Account Info";
+            ConsoleUtil.DisplayReset();
+
+            ConsoleUtil.DisplayMessage("First Name: " + salesperson.FirstName);
+            ConsoleUtil.DisplayMessage("Last Name: " + salesperson.LastName);
+            ConsoleUtil.DisplayMessage("Account ID: " + salesperson.AccountID);
+
+            DisplayContinuePrompt();
+        }
+
+
+    }
+
+}
